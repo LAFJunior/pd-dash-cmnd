6b4f7cff2581e132fcda39d9a33dbf128e2c632e
Luiz A. F. Junior <160649429+LAFJunior@users.noreply.github.com>
2025-07-07 10:55:01 -0300

    Update EstruturaDepartamento.tsx

--- src/components/departamento/EstruturaDepartamento.tsx
+++ src/components/departamento/EstruturaDepartamento.tsx
@@ -87,7 +87,7 @@ const EstruturaDepartamento: React.FC<{ departamento: string }> = ({ departament
       <div className="p-8">
         {/* Nível 1 - Gerência */}
         <div className="flex justify-center mb-4">
-        <div className="w-[100px]">
+        <div className="w-[300px]">
           <Colaborador 
             nome="Márcio Sampaio" 
             cargo="Diretor" 
@@ -98,7 +98,6 @@ const EstruturaDepartamento: React.FC<{ departamento: string }> = ({ departament
         </div>
         {/* Nível 2 - Coordenação */}
         <div className="flex justify-center gap-1 mb-4">
-        <div className="w-[100px]">
           <Colaborador 
             nome="Flávia Araujo" 
             cargo="Gerente" 
